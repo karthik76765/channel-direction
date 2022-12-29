c9682e81c25766e9d1c1a82314df5b013f1fe888
karthik <119947493+karthik76765@users.noreply.github.com>
2022-12-29 19:41:15 +0530

    Add files via upload

---
+++ channel direction.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func ping(ping <- string, msg string) {
+	pings <- msg
+}
+func pong(ping <- chan string,pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
+func main() {
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
+}
